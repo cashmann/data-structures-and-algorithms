824dfdc91cd1287ecf09216a1af76b7c68022631
‘Nathan <ncash13@hotmail.com>
2018-08-14 15:33:14 -0500

    finished array-shift

---
+++ array-shift/__test__/lib/array-shift.js
@@ -0,0 +1,21 @@
+var insertShiftArray = (arr, el) => {
+  if(!Array.isArray(arr)){
+    return null;
+  }else if(!el){
+    return arr;
+  }else{
+    var newArr = [];
+    var insertPoint = Math.ceil(arr.length/2);
+    for(let i=0; i<arr.length; i++){
+      if(i===insertPoint){
+        newArr[i] = el;
+      }else if(i>insertPoint){
+        newArr[i]= arr[i];
+      }else{
+        newArr[i] = arr[i+1];
+      }
+    } return newArr;
+  }
+}
+
+module.exports = insertShiftArray;
