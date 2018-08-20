7486e1d11f23aebdff99751645909fe0ab519e4b
‘Nathan <ncash13@hotmail.com>
2018-08-20 15:56:33 -0500

    added append, insertAfter/Before functions

--- linked_list/__tests__/linked-list.test.js
+++ linked_list/__tests__/linked-list.test.js
@@ -14,6 +14,7 @@ describe('linkedList', ()=>{
       expect(list.head).toEqual({'data': 'hi', 'next': null});
       list.insert({'data': 'hi', 'next': null});
       expect(list.head).toEqual({'data': {'data': 'hi', 'next': null}, 'next': {'data': 'hi', 'next': null}});
+      expect(list.itemCount).toBe(2);
     });
     it('can check if a value is in the list', ()=>{
       var list = new linkedList();
@@ -23,6 +24,40 @@ describe('linkedList', ()=>{
       expect(list.includes('hi')).toBe(true);
       expect(list.includes('bye')).toBe(true);
       expect(list.includes(1)).toBe(false);
+    });
+    it('can insert a new value at the end of a list', ()=>{
+      var list = new linkedList();
+      list.insert('hi');
+      list.insert('bye');
+      list.insert('hola');
+      list.append('pfft');
+      expect(list.itemCount).toBe(4);
+      expect(list.head.next.next.next).toEqual({'data': 'pfft', 'next': null});
+    });
+    it('can insert a new value after a given value', ()=>{
+      var list = new linkedList();
+      list.insert('hi');
+      list.insert('bye');
+      list.insert('hola');
+      list.append('pfft');
+      list.insertAfter('bye', 1);
+      expect(list.itemCount).toBe(5);
+      expect(list.head.next.next.data).not.toBe('hi');
+      expect(list.head.next.next.data).toBe(1);
+      expect(list.head.next.next.next.data).toBe('hi');
+    });
+    it('can insert a new value before a given value', ()=>{
+      var list = new linkedList();
+      list.insert('hi');
+      list.insert('bye');
+      list.insert('hola');
+      list.append('pfft');
+      list.insertBefore('hola', 5);
+      expect(list.itemCount).toBe(5);
+      expect(list.head.data).toBe(5);
+      expect(list.head.next.data).toBe('hola');
+      list.insertBefore('hola', 6);
+      expect(list.head.next.data).toBe(6);
     });
   });
 });
--- linked_list/linked-list.js
+++ linked_list/linked-list.js
@@ -10,11 +10,64 @@ class Node{
 class LinkedList{
   constructor(){
     this.head = null;
+    this.itemCount = 0;
   }
   insert(val){
     let newHead = new Node(val);
     newHead.next = this.head;
     this.head = newHead;
+    this.itemCount++;
+  }
+
+  append(val){
+    let current = this.head;
+    for(let i = 1; i< this.itemCount; i++){
+      current = current.next;
+    }
+    current.next = new Node(val);
+    this.itemCount++;
+  }
+  insertAfter(val, newVal){
+    let current = this.head;
+    do{
+      if(current.data === val){
+        newVal = new Node(newVal);
+        newVal.next = current.next;
+        current.next = newVal;
+        this.itemCount++;
+        return this;
+      }
+      else{
+        current = current.next;
+      }
+    } while(current);
+    console.error('Insertion point not found.');
+  }
+  insertBefore(val, newVal){
+    var current = this.head;
+    if(val === current.data){
+      this.insert(newVal);
+      return this;
+    }
+    let valPos = 0;
+    do{
+      if(current.data === val){
+        let newValNode = new Node(newVal);
+        current = this.head;
+        for(let i=0; i<valPos-1; i++){
+          current = current.next;
+        }
+        newValNode.next = current.next;
+        current.next = newValNode;
+        this.itemCount++;
+        return this;
+      }
+      else{
+        current = current.next;
+        valPos++;
+      }
+    }while(current);
+    console.error('Insertion point not found.');
   }
   includes(val){
     let current = this.head;
